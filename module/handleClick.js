9966f754db420a22538048aba2426eccc9c6f518
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2022-10-11 10:59:58 +0530

    prevented constinues button click

--- module/handleClick.js
+++ module/handleClick.js
@@ -1,13 +1,16 @@
 import fetchJokes from "../module/fetchData.js";
-import { jokeContainer } from "../module/elements.js";
+import { jokeContainer, button } from "../module/elements.js";
 import preLoader from "../module/preloader.js";
 
 export default async function handleClick() {
     const url = "https://icanhazdadjoke.com/";
+    console.log(this);
+    this.disabled = true;
     jokeContainer.textContent = '';
     jokeContainer.append(preLoader());
     const response = await fetchJokes(url);
     if (!response) return;
     const joke = response.joke;
     jokeContainer.textContent = joke;
+    this.disabled = false;
 }
